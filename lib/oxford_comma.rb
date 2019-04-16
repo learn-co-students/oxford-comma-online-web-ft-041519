c40934ec7d9effa88717dc4c6b659c8a62f36df0
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 01:23:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    new_arr = array[array.length - 1]
+    array.pop
+    array.join(", ")
+  else
 
+  end
 end
