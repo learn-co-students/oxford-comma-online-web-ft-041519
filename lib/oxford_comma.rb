0e96dcb291ed3dd6289d6ebe5d900eae58fb6eaf
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-24 07:50:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
 def oxford_comma(array)
-  if array.length == 1
+   if array.length == 1
     return "#{array[0]}"
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
-    new_last_array_item = "and #{array[-1]}"
-    array.pop
-    array.push(new_last_array_item)
+    array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
 end
